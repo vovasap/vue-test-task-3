779025afad64f2320b04df9182627014ee9243f7
vovasap <rednotgreen2020@gmail.com>
2021-09-12 22:27:09 +0300

    feat: add display of items

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,71 +1,99 @@
 <template>
   <div class="settings">
-    <pre>{{citiesWeather}}</pre>
-    <p class="settings_title">Settings</p>
-    <label class="settings_input-title" for="location">Add Location:</label>
+    <!-- <pre>{{ citiesWeather }}</pre> -->
+    <settings-item
+      v-for="cityWeather in citiesWeather"
+      :city="cityWeather.name"
+      :country="cityWeather.sys.country"
+      :id="cityWeather.id"
+      @deleteItem="deleteItem"
+      :key="cityWeather.id"
+    />
+    <p class="settings__title">Settings</p>
+    <label class="settings__input-title" for="location">Add Location:</label>
     <div>
-      <input id="location" class="settings_input" type="text" v-model="query">
-      <w-button icon="arrow-return-left" @click="addLocation"/>
+      <input
+        id="location"
+        class="settings__input"
+        type="text"
+        v-model="query"
+      />
+      <w-button icon="arrow-return-left" @click="addLocation" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import WButton from '@/components/WButton.vue'
+import SettingsItem from '@/components/SettingsItem.vue'
 import { ref } from 'vue'
 
 export default {
   name: 'settings',
   components: {
     WButton,
+    SettingsItem,
   },
   props: {
     value: {
       type: Array,
-      default: () => ([])
-    }
+      default: () => [],
+    },
   },
-  setup(props: { value: Array<Record<string, any>> }, context: any) {
+  setup(props: any, context: any) {
     const { emit } = context
-    const citiesWeather = ref<Array<Record<string,any>>>(props.value)
+    const citiesWeather = ref<Array<Record<string, any>>>(props.value)
     const query = ref<string>('pskov')
 
-    const getWeatherByLocation = async (location: string): Promise<Record<string, any>> => {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}`)
+    const getWeatherByLocation = async (
+      location: string
+    ): Promise<Record<string, any> | null> => {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}`
+      )
       if (response.status === 200) {
         return await response.json()
       } else {
         //TODO handle error
-        return new Promise(resolve => resolve({}))
+        return new Promise((resolve) => resolve(null))
       }
     }
 
     const addLocation = (): void => {
-      getWeatherByLocation(query.value).then(weather => {
-        citiesWeather.value.push(weather)
-        emit('input', citiesWeather)
-        query.value = ''
+      getWeatherByLocation(query.value).then((weather) => {
+        if (weather) {
+          citiesWeather.value.push(weather)
+          emit('input', citiesWeather.value)
+          query.value = ''
+        }
       })
+    }
+
+    const deleteItem = (id: number): void => {
+      citiesWeather.value = citiesWeather.value.filter((city) => city.id !== id)
+      emit('input', citiesWeather.value)
     }
 
     return {
       query,
       addLocation,
       citiesWeather,
+      deleteItem,
     }
-  }
+  },
 }
 </script>
 <style lang="scss">
-.settings{
- max-width: 320px;
+.settings {
+  max-width: 320px;
 
-  &_title, &_input-title {
-   font-weight: 700;
+  &__title,
+  &__input-title {
+    font-weight: 700;
   }
 
-  &_input {
-   padding: 4px;
+  &__input {
+    padding: 4px;
   }
 }
 </style>
---
+++ src/components/SettingsItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="settings-item">
+    <w-button icon="list" size="18" />
+    <span class="settings-item__title">{{ `${city}, ${country}` }}</span>
+    <w-button icon="trash" size="16" @click="deleteItem" />
+  </div>
+</template>
+<script lang="ts">
+import WButton from './WButton.vue'
+
+export default {
+  components: {
+    WButton,
+  },
+  props: {
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props: any, context: any) {
+    const { emit } = context
+    const deleteItem = (): void => {
+      emit('deleteItem', props.id)
+    }
+
+    return {
+      deleteItem,
+    }
+  },
+}
+</script>
+<style lang="scss">
+.settings-item {
+  display: flex;
+  align-items: center;
+  margin-bottom: 4px;
+  padding: 2px;
+
+  background: #ccb;
+
+  &__title {
+    flex: 1 0 auto;
+  }
+}
+</style>
